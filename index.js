abd6184d48415a32e5d26c30491dc42c109c7282
Shafa Alafghany <alafghanyshafa@gmail.com>
2021-02-28 11:32:31 +0700

    Add CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 require('dotenv').config()
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
